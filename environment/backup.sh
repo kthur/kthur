042f6155834e8f8abb74f5a031ab669412815ca6
KYUNGTAK HUR <kyungtak.hur@gmail.com>
2021-02-01 00:14:22 +0900

    update backup

--- environment/backup.sh
+++ environment/backup.sh
@@ -24,7 +24,8 @@ then
 	sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600sudo
 	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pubsudo
 	sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-	sudo apt-get updatesudo apt-get -y install cuda
+	sudo apt-get update
+	sudo apt-get -y install cuda
 else
 	echo "b: backup"
 	echo "r: restore"
